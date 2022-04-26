84700d0b4cc867f4496d0a8f36447f9892609470
Jeong Jinwoo <jwjeong96@gmail.com>
2022-04-26 23:48:01 +0900

    test(dao): change test db structure
    - add dummy data at test db
    - cleanse and fix broken dao tests

--- src/test/java/chess/dao/GameDaoTest.java
+++ src/test/java/chess/dao/GameDaoTest.java
@@ -29,10 +29,7 @@ class GameDaoTest {
     void findAll_메서드로_존재하는_모든_게임_정보를_조회가능() {
         List<GameEntity> actual = dao.findAll();
 
-        List<GameEntity> expected = List.of(
-                new GameEntity(1, "진행중인_게임", true),
-                new GameEntity(2, "종료된_게임", false),
-                new GameEntity(3, "이미_존재하는_게임명", true));
+        List<GameEntity> expected = getAllTestData();
 
         assertThat(actual).isEqualTo(expected);
     }
@@ -76,14 +73,14 @@ class GameDaoTest {
     void countAll_메서드로_여태까지_저장된_모든_데이터의_개수_조회가능() {
         int actual = dao.countAll();
 
-        assertThat(actual).isEqualTo(3);
+        assertThat(actual).isEqualTo(5);
     }
 
     @Test
     void countRunningGames_메서드로_running값이_참인_데이터의_개수_조회가능() {
         int actual = dao.countRunningGames();
 
-        assertThat(actual).isEqualTo(2);
+        assertThat(actual).isEqualTo(4);
     }
 
     @DisplayName("saveAndGetGeneratedId 메서드는 게임 저장 후 id값 반환")
@@ -95,7 +92,9 @@ class GameDaoTest {
             int actual = dao.saveAndGetGeneratedId(
                     new EncryptedAuthCredentials("name", "passwordHash"));
 
-            assertThat(actual).isGreaterThan(3);
+            int previousDataCount = getAllTestData().size();
+
+            assertThat(actual).isGreaterThan(previousDataCount);
         }
 
         @Test
@@ -146,5 +145,14 @@ class GameDaoTest {
             assertThatThrownBy(() -> dao.deleteGame(existingGameData))
                     .hasMessage("게임을 삭제하는 데 실패하였습니다!");
         }
+    }
+
+    private List<GameEntity> getAllTestData() {
+        return List.of(
+                new GameEntity(1, "진행중인_게임", true),
+                new GameEntity(2, "종료된_게임", false),
+                new GameEntity(3, "이미_존재하는_게임명", true),
+                new GameEntity(4, "참여자가_있는_게임", true),
+                new GameEntity(5, "참여자가_없는_게임", true));
     }
 }
